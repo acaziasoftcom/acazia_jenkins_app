42c7e5075a4c5e45a211964c4c586962977c9dc0
Quan Pham <doraemonfanclub@gmail.com>
2019-12-31 02:07:46 +0700

    Added first tabs

---
+++ lib/screens/tab-container/tab_layout.dart
@@ -0,0 +1,25 @@
+// This is actually a wrapper for another 5 screens: Guard, Profile, Store, Chat, Account
+// It should have its own navigator.
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TabLayout extends StatefulWidget {
+  @override
+  _TabLayoutState createState() => _TabLayoutState();
+}
+
+class _TabLayoutState extends State<TabLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: FancyBottomNavigation(
+          initialSelection: 1,
+          circleColor: Colors.amber,
+          tabs: [
+      TabData(iconData: Icons.home, title: "Statistic"),
+      TabData(iconData: Icons.dashboard, title: "Dashboard"),
+      TabData(iconData: Icons.shopping_cart, title: "Nodes")
+    ], onTabChangedListener: (position) {}));
+  }
+}
