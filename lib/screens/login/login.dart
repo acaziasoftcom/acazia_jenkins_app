5f887a9b89481bc012ae51a758f4bdd76ba63cfe
Quan Pham <doraemonfanclub@gmail.com>
2019-12-31 11:14:06 +0700

    Added login skeleton

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,4 +1,6 @@
+import 'package:acazia_jenkins/common/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,8 +10,113 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 42, horizontal: 50),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              AJColors.DARK_BLUE,
+              AJColors.ALMOST_BLACK
+            ])),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.fromLTRB(30, 46, 30, 38),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: AJColors.DARK_BLUE,
+                    offset: Offset(0.0, 0.0),
+                    blurRadius: 5.0,
+                    spreadRadius: 7.0)
+              ], shape: BoxShape.circle),
+              child: Icon(
+                FontAwesomeIcons.jenkins,
+                color: Colors.white,
+                size: 86,
+              ),
+            ),
+            SizedBox(
+                child: Column(
+              children: <Widget>[
+                TextField(
+                  style: TextStyle(color: Colors.white60),
+                  decoration: InputDecoration(
+                      counterText: '',
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: AJColors.GUNMETAL)),
+                      border: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: AJColors.GUNMETAL)),
+                      hintText: 'Username',
+                      hintStyle: TextStyle(color: AJColors.GUNMETAL)),
+                  maxLength: 32,
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextField(
+                  obscureText: true,
+                  style: TextStyle(color: Colors.white60),
+                  decoration: InputDecoration(
+                      counterText: '',
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: AJColors.GUNMETAL)),
+                      border: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: AJColors.GUNMETAL)),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: AJColors.GUNMETAL)),
+                  maxLength: 32,
+                ),
+                ConstrainedBox(
+                  constraints: BoxConstraints(minWidth: 120, maxWidth: 230),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                          padding: const EdgeInsetsDirectional.only(top: 62)),
+                      FlatButton(
+                        color: AJColors.DARK_BLUE_GREY,
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsetsDirectional.only(top: 16)),
+                      FlatButton(
+                        color: AJColors.LIGHT_NAVY_BLUE,
+                        onPressed: () {},
+                        child: Text(
+                          'Signup',
+                          style: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsetsDirectional.only(top: 42)),
+                      FlatButton(
+                        color: Colors.transparent,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'Go Back',
+                          style: TextStyle(color: Colors.white54),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
